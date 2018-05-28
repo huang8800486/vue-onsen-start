0c8eda4c12f46bcbddb504b9e7d6591ecbf860b8
TonyChen <tonychenn@gmail.com>
2018-05-28 14:12:22 +0800

    加入login page

---
+++ src/pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="left">
+        <v-ons-back-button>回上頁</v-ons-back-button>
+      </div>
+      <div class="center">綁定帳號</div>
+    </v-ons-toolbar>
+    <v-ons-list>
+        <v-ons-list-item>
+            <div class="center">
+                <v-ons-input placeholder="請輸入員工編號" float
+                v-model="emno"
+                >
+                </v-ons-input>
+            </div>
+        </v-ons-list-item>
+        <v-ons-list-item>
+            <div class="right">
+                Hello {{ emno || 'anonymous' }}!
+            </div>
+        </v-ons-list-item>
+    </v-ons-list>
+  </v-ons-page>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            emno: '',
+            password: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,27 @@
+import Login from '../pages/login';
 import Dashboard from '../pages/dashboard';
 import SubscribeNotify from '../pages/subscribeNotify';
 
-const routes = [
-    {
+const routes = [{
         path: '',
         name: 'dashboard',
         component: {
             extends: Dashboard
         },
-        children: [
-            {
-                path: 'subscribeNotify',
-                name: 'subscribeNotify',
-                component: {
-                    extends: SubscribeNotify
-                }
-            }        
-        ]
+        children: [{
+            path: 'subscribeNotify',
+            name: 'subscribeNotify',
+            component: {
+                extends: SubscribeNotify
+            }
+        }]
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: {
+            extends: Login
+        },
     }
 ];
 
